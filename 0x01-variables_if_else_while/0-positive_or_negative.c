02c6ead493c8682a94c6bd6eddb2d29fdda4e893
Fadilat Abdulaziz Abubakar <jyahaya50@gmail.com>
2022-10-17 16:37:45 -0700

    a code that prints numbers

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Assigns a random number to a variable
+*Return: 0
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+print("%d is positive\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+else
+printf("%d is zero\n", n);
+return (0);
+}
