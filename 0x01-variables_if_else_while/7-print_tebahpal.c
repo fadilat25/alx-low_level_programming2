31e317189182f0a2e53356a519812d7a3c2870c8
Fadilat Abdulaziz Abubakar <jyahaya50@gmail.com>
2022-10-18 02:12:21 -0700

    prints lowercase alphabets in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpal.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in reverse order
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+c = 'z';
+while
+(c >= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in reverse order
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char b;
+b = 'z';
+while
+(b >= 'a') {
+putchar(b);
+b--;
+}
+putchar('\n');
+return (0);
+}
