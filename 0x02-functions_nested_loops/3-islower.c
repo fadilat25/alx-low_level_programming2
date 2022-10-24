7b36231421ff8fff5f0416f619a5f0c979b43521
Fadilat Abdulaziz Abubakar <jyahaya50@gmail.com>
2022-10-23 17:43:58 -0700

    a code to check if a letter is lowercase or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *islower - checks if  a character is a lowercase character
+ *
+ *Return: returns a value based on preset conditions
+ */
+int islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
