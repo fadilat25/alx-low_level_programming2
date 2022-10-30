437756f8269801bf6a2d9febad13ec9e2a0798f9
Fadilat Abdulaziz Abubakar <jyahaya50@gmail.com>
2022-10-30 03:58:43 -0700

    Edited

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *print_sign - prints 
+ *print_sign - prints
  *
  *Return: returns 0, 1, -1, based on conditions given
  */
@@ -10,16 +10,16 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar('+');
-return (1);     
+return (1);
 }
 else if (n == 0)
 {
 _putchar('0');
-return(0);
+return (0);
 }
 else
 {
 _putchar('-');
-return(-1);
+return (-1);
 }
 }
