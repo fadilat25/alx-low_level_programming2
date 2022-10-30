a873b2860f9cd54ef0bc00c3b58795d7a0cd1bc0
Fadilat Abdulaziz Abubakar <jyahaya50@gmail.com>
2022-10-30 04:32:40 -0700

    Rteurns absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - absolute value of a number
+ *
+ *@n : parameter
+ *
+ *Return: returns absolute value of a num
+ */
+int _abs(int n)
+{
+if (n < 0)
+n = -n;
+return (n);
+}
