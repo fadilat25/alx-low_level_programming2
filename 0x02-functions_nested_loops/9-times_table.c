c332dc4ed0b8259bd13ef15c6db5df7b35f18e65
Fadilat Abdulaziz Abubakar <jyahaya50@gmail.com>
2022-10-30 10:02:35 -0700

    prints the times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,8 +17,8 @@ prod = num * multi;
 if (prod <= 9)
 _putchar(' ');
 else
-_putchar((prod / 10) * '0');
-_putchar((prod % 10) * '0');
+_putchar((prod / 10) + '0');
+_putchar((prod % 10) + '0');
 }
 _putchar('\n');
 }
