e84bcac7556acea41ee1afa76061d2930ee09935
Fadilat Abdulaziz Abubakar <jyahaya50@gmail.com>
2022-10-30 04:52:40 -0700

    prints absolute value

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit of a number
+ *@n: arguments
+ *
+ *Return: the last digit
+ */
+int print_last_digit(int n)
+{
+int x;
+if (n > 0)
+n = -n;
+x = n % 10;
+_putchar(x + '0');
+return (x);
+}
