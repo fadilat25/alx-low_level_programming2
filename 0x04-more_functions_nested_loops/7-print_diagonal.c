2b0de24078d21b440b967229f45d8180f4933505
Fadilat Abdulaziz Abubakar <jyahaya50@gmail.com>
2022-10-30 17:05:13 -0700

    prints a diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal
+ * @n: number of slashes to be printed
+ */
+void print_diagonal(int n)
+{
+int i, j;
+if (n <= 0)
+_putchar('\n');
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
+}
+}
